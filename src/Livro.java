f1dcde9a65e251c6cf18d5544514c0e625a9e5fc
Rochasg7 <gabrielrocha.dev2025@gmail.com>
2026-08-24 17:31:58 -0300

    get e set livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,18 +1,60 @@
 public class Livro {
 
-    int idLivro;
-    String titulo;
-    String autor;
-    String status;
+    private int idLivro;
+    private String titulo;
+    private String autor;
+    private String status;
 
-    void alterarStatus(String status) {
-        this.status = status;
+    public Livro(int idLivro, String titulo, String autor) {
+        this.idLivro = idLivro;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.status = "Disponível";
     }
 
-    void exibirInformacoes() {
-        System.out.println("ID: " + idLivro);
-        System.out.println("Título: " + titulo);
-        System.out.println("Autor: " + autor);
-        System.out.println("Status: " + status);
+    public int getIdLivro() {
+        return idLivro;
+    }
+
+    public void setIdLivro(int idLivro) {
+        this.idLivro = idLivro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        if (status.equals("Disponível") || status.equals("Emprestado")) {
+            this.status = status;
+        } else {
+            System.out.println("Status inválido. Use 'Disponível' ou 'Emprestado'.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "idLivro=" + idLivro +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
